e9707d87f73a267c9e953390e5131de583b927d6
TruffleWorm <rishay.raghav00@gmail.com>
2023-03-29 09:40:09 +0530

    changed connection and serving

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ require('dotenv').config()
 
 const port = process.env.PORT;
 
-const mongoURL = "mongodb+srv://"+ process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@cluster0.v3owmde.mongodb.net/" + process.env.DB_NAME + "?retryWrites=true&w=majority"
+const mongoURL = process.env.MONGO_URL
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-mongoose.connect(mongoURL).catch(err => console.log(err))
+mongoose.connect(mongoURL)
+  .then(() => {
+    app.listen(port, function () {
+      console.log(`Server started on port http://localhost:${port}`);
+    });
+  })
+  .catch(err => console.log(err))
 
 const postsSchema = mongoose.Schema({
   title: {
@@ -36,7 +42,6 @@ app.use(express.static("public"));
 app.get('/', async (req, res) => {
   await Posts.find({}).maxTimeMS(30000)
     .then(posts => {
-      console.log(posts);
       res.render('home', { homeStartingContent: homeStartingContent, posts: posts });
     })
 });
@@ -74,17 +79,11 @@ app.post('/compose', (req, res) => {
 
 app.get('/posts/:postTitle', (req, res) => {
   const requestedTitle = req.params.postTitle;
-  Posts.find({_id: requestedTitle})
+  Posts.find({ _id: requestedTitle })
     .then(posts => {
-      res.render('post', {postTitle: posts[0].title, postBody: posts[0].body})
+      res.render('post', { postTitle: posts[0].title, postBody: posts[0].body })
     })
     .catch(() => {
       res.render('404');
     })
 })
-
-
-
-  app.listen(port, function () {
-    console.log('Server started on port http://localhost:' + port);
-  });
